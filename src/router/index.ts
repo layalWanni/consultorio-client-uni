456ea2a1765938956d356ba030ede505f9b4935c
Layal Youssef Safa Al Wanni <nissolelo03@gmail.com>
2022-06-28 18:12:33 -0300

    update03

--- src/router/index.ts
+++ src/router/index.ts
@@ -14,6 +14,41 @@ const routes: Array<RouteRecordRaw> = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  },
+  {
+    path: '/secretarias',
+    name: 'secretarias',
+    component: () => import(/* webpackChunkName: "about" */ '../views/secretaria/secretaria-list.vue')
+  },
+  {
+    path: '/pacientes',
+    name: 'pacientes',
+    component: () => import(/* webpackChunkName: "about" */ '../views/paciente/paciente-list.vue')
+  },
+  {
+    path: '/medicos',
+    name: 'medicos',
+    component: () => import(/* webpackChunkName: "about" */ '../views/medico/medico-list.vue')
+  },
+  {
+    path: '/historicos',
+    name: 'historicos',
+    component: () => import(/* webpackChunkName: "about" */ '../views/historico/historico-list.vue')
+  },
+  {
+    path: '/especialidades',
+    name: 'especialidades',
+    component: () => import(/* webpackChunkName: "about" */ '../views/especialidade/especialidade-list.vue')
+  },
+  {
+    path: '/convenios',
+    name: 'convenios',
+    component: () => import(/* webpackChunkName: "about" */ '../views/convenio/convenio-list.vue')
+  },
+  {
+    path: '/agendamentos',
+    name: 'agendamentos',
+    component: () => import(/* webpackChunkName: "about" */ '../views/agendamento/agendamento-list.vue')
   }
 ]
 
